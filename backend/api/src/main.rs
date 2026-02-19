1b489025ae06b4acd44351ac082b79b7639771e4
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2026-02-19 20:21:58 +0530

    adding poem

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,6 +1,16 @@
+use poem::{IntoResponse, Route, Server, get, handler, listener::TcpListener, web::Path};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/hello/:name", get(hello))
+        .at("/hello", get(|| async { "hello".into_response() }));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
-    
