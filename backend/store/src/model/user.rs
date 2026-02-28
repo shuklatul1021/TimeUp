c8a9483a0dee1040439c97756959da5702e663d5
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2026-02-28 21:51:42 +0530

    complete User Route

--- backend/store/src/model/user.rs
+++ backend/store/src/model/user.rs
@@ -1,10 +1,53 @@
-use crate::store::Store;
+use diesel::{prelude::*};
+use uuid::Uuid;
+use crate::{schema::user::username, store::Store};
+
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::user)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+
+struct User {
+    id : String,
+    username : String,
+    password : String,
+}
+
 
 impl Store {
-    pub fn register(&self) {
+    pub fn register(&mut self , input_username : String , input_password : String ) -> Result<String , diesel::result::Error> {
+        let id = Uuid::new_v4();
+        let u = User {
+            id : id.to_string(),
+            username : input_username,
+            password : input_password
+        };
+        
+        diesel::insert_into(crate::schema::user::table)
+            .values(&u)
+            .returning(User::as_returning())
+            .get_result(&mut self.connect)
+            .expect("Error while creating user");
 
+        Ok(id.to_string())
+        
     } 
-    pub fn login(&self) {
+    pub fn login(&mut self , input_username : String , input_password : String) -> Result<bool , diesel::result::Error> {
+        let users = crate::schema::user::table
+            .filter(username.eq(input_username))
+            .select(User::as_select())
+            .load(&mut self.connect)
+            .expect("Error loading User");
 
+        if users.len() <= 0 {
+            return Ok(false);
+        }
+        for u in users{
+            if u.password != input_password {
+                return Ok(false);
+            }
+        }
+
+        Ok(true)
     }
 }
--- backend/store/src/model/website.rs
+++ backend/store/src/model/website.rs
@@ -1,4 +1,16 @@
-use crate::store::Store;
+use diesel::{prelude::*};
+use uuid::Uuid;
+use crate::{store::Store};
+
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::website)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+
+struct Website {
+    id : String,
+}
+
 
 impl Store {
     pub fn create_website(&self) {
