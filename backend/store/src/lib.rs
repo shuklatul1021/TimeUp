aeaade30bbf37e28328a35fdf4f0af50e1e2f5dc
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2026-02-28 18:18:47 +0530

    adding store

--- backend/store/src/lib.rs
+++ backend/store/src/lib.rs
@@ -1,10 +1,18 @@
+use diesel::Connection;
+
 
 pub struct Store{
-
+    conn :  Connection
 }
-
-
-
+//Constructor for Store
+impl Default for Store{
+    fn default() -> Self {
+        Self {
+            conn
+        }
+    }
+}
+//Implementation of Store
 impl Store {
     pub fn create(&self) {
 
