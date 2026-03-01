347f297d9dde55c1527d33c60f51e70c75025734
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2026-03-01 17:50:16 +0530

    adding arc and mutex

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,45 +1,53 @@
-use poem::{Route, Server, get, handler, listener::TcpListener, post, web::{ Json, Path}};
+use std::sync::{Arc, Mutex};
+
+use poem::{EndpointExt, Route, Server, get, handler, listener::TcpListener, post, web::{ Data, Json, Path}};
 use store::store::Store;
 use crate::{request_input::{CreateUserAuthRequest, CreateWebsiteRequest}, request_output::{CreateUserLogedInResponse, CreateUserRegestrationResponse, CreateWebsiteResponse}};
 
 pub mod request_input;
 pub mod request_output;
 
-
 #[handler]
-fn get_website(Path(name): Path<String>) -> String {
-    let s = Store::default();
-    return format!("Hello, {}!", name);
-
+fn get_website(Path(name): Path<String> , Data(s) : Data<&Arc<Mutex<Store>>>) -> String {
+    let mut s = s.lock().unwrap();
+    let web = s.get_website(name).unwrap();
+    return format!("Hello, {}", web.id);
 } 
 #[handler]
-fn create_website(Json(data) : Json<CreateWebsiteRequest>) -> Json<CreateWebsiteResponse> {
-    let url = data.url;
+fn create_website(Json(data) : Json<CreateWebsiteRequest>, Data(s) : Data<&Arc<Mutex<Store>>>) -> Json<CreateWebsiteResponse> {
+    let mut _s = s.lock().unwrap();
+    println!("Creating website with URL: {}", data.url);
     return Json(CreateWebsiteResponse { id: String::from("12345") });
 }
 
 #[handler]
-fn register_user(Json(data) : Json<CreateUserAuthRequest>) -> Json<CreateUserRegestrationResponse> {
+fn register_user(Json(data) : Json<CreateUserAuthRequest> ,  Data(s) : Data<&Arc<Mutex<Store>>>) -> Json<CreateUserRegestrationResponse> {
+    let mut _s = s.lock().unwrap();
     let username = data.username;
     let password = data.password;
+    println!("Username: {}, Password: {}", username, password);
     return Json(CreateUserRegestrationResponse { message : String::from("Success") , success : true });
 }
 
 #[handler]
-fn login_user(Json(data) : Json<CreateUserAuthRequest>) -> Json<CreateUserLogedInResponse> {
+fn login_user(Json(data) : Json<CreateUserAuthRequest>, Data(s) : Data<&Arc<Mutex<Store>>>) -> Json<CreateUserLogedInResponse> {
+    let mut _s = s.lock().unwrap();
     let username = data.username;
     let password = data.password;
+    println!("Username: {}, Password: {}", username, password);
     return Json(CreateUserLogedInResponse { token : String::from("cjnxncxcxcxmccmxcnxcm")});
 }
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let s = Arc::new(Mutex::new(Store::default().unwrap()));
     let app = Route::new()
         .at("/auth/register", post(register_user))
         .at("/auth/log-in", post(login_user))
         .at("/website/:website_id", get(get_website))
-        .at("/website", post(create_website));
+        .at("/website", post(create_website))
+        .data(s);
 
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
