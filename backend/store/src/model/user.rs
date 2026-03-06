a3252016ac10e7df6fc77da8493ebf15edce2c46
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2026-03-06 13:05:38 +0530

    fixing struct in store

--- backend/store/src/model/user.rs
+++ backend/store/src/model/user.rs
@@ -1,38 +1,43 @@
-use diesel::{prelude::*};
+use crate::store::Store;
+use diesel::prelude::*;
 use uuid::Uuid;
-use crate::{store::Store};
-
 
 #[derive(Queryable, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::user)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 
 struct User {
-    id : String,
-    username : String,
-    password : String,
+    id: String,
+    username: String,
+    password: String,
 }
 
-
 impl Store {
-    pub fn register(&mut self , input_username : String , input_password : String ) -> Result<String , diesel::result::Error> {
+    pub fn register(
+        &mut self,
+        input_username: String,
+        input_password: String,
+    ) -> Result<String, diesel::result::Error> {
         let id = Uuid::new_v4();
         let u = User {
-            id : id.to_string(),
-            username : input_username,
-            password : input_password
+            id: id.to_string(),
+            username: input_username,
+            password: input_password,
         };
-        
+
         diesel::insert_into(crate::schema::user::table)
             .values(&u)
             .returning(User::as_returning())
             .get_result(&mut self.connect)
-            .expect("Error while creating user");
+            .expect("Error while creating user Account");
 
         Ok(id.to_string())
-        
-    } 
-    pub fn login(&mut self , input_username : String , input_password : String) -> Result<bool , diesel::result::Error> {
+    }
+    pub fn login(
+        &mut self,
+        input_username: String,
+        input_password: String,
+    ) -> Result<(bool, String), diesel::result::Error> {
         use crate::schema::user::dsl::*;
         let users = user
             .filter(username.eq(input_username))
@@ -41,14 +46,15 @@ impl Store {
             .expect("Error loading User");
 
         if users.len() <= 0 {
-            return Ok(false);
+            return Ok((false, String::from("User not found")));
         }
-        for u in users{
+        let user_id = users[0].id.clone();
+        for u in users {
             if u.password != input_password {
-                return Ok(false);
+                return Ok((false, String::from("Invalid password")));
             }
         }
 
-        Ok(true)
+        Ok((true, user_id))
     }
 }
